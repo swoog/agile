74ecf73be6b0ae23f23d399b4c4e2a1928551fcb
Aurelien GALTIER <aurelien.galtier@live.fr>
2014-09-10 12:28:34 +0200

    Add apges

---
+++ Cellenza.Quizz/Cellenza.Quizz/Cellenza.Quizz/QuizzPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cellenza.Quizz
+{
+    public partial class QuizzPage
+    {
+        public QuizzPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
